6c292851df06c9fe11c93a44a1c423bf810eae30
ChilasDipo <Chil0041@edu.easj.dk>
2020-09-21 13:31:01 +0200

    lavede fail i test case runner

--- src/Kapitel_2/Task1.java
+++ src/Kapitel_2/Task1.java
@@ -1,17 +1,14 @@
 package Kapitel_2;
-
 import java.util.Scanner;
-
 public class Task1 {
     public static void main(String[] args) {
         System.out.print("Input længde i miles ");
         Scanner input = new Scanner(System.in);
         double miles = Integer.parseInt(input.next());
         double KM = miles * 1.6 ;
-
         System.out.println("Længden i Kilometer er " + KM);
-
-
-
+    }
+    public static double test(double miles) {
+        return miles * 1.6 ;
     }
 }
--- src/Kapitel_2/Test.java
+++ src/Kapitel_2/Test.java
@@ -1,13 +1,15 @@
 package Kapitel_2;
-
 public class Test {
     public static void main(String[] args) {
-        TestTask6();
+        TestTask1();
     }
-
-    public static void TestTask6() {
-
-
-
+    public static void TestTask1() {
+        for (int i = 0; i < 101; i++) {
+            if (Task1.test(i) == i * 1.6) {
+                System.out.println("True for    " + i);
+            } else {
+                System.out.println("Nope for   " + i);
+            }
+        }
     }
 }
