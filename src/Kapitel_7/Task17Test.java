4442eb2578c87d5dd90d447c64ef3c21f0f5bdad
Chilas Main PC <Chil0041@edu.easj.dk>
2020-11-03 16:26:48 +0100

    not done at all :D

---
+++ src/Kapitel_7/Task17Test.java
@@ -0,0 +1,16 @@
+package Kapitel_7;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Task17Test {
+
+
+    @Test
+    void sorting(){
+        Task17 opgave = new Task17();
+       // assertEquals(opgave.sorting,"Gustav","Chilas","Jens");
+    }
+
+}
